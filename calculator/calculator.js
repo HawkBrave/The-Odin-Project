a542d45450d5499e5915406b60757fcb755f63d2
HawkBrave <doganyigityilmazz@gmail.com>
2020-07-16 17:03:28 +0300

    calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,9 @@
+const calculator = document.querySelector("#calculator");
+const keys = document.querySelector("#keys");
+
+/*for (let i = 9; i >= 0; i--) {
+  let numericKey = document.createElement("div");
+  numericKey.setAttribute("class", 'key');
+  numericKey.innerHTML = i;
+  keys.appendChild(numericKey);
+}*/
